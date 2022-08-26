2704d33ade01c8456ea1dba12012c4c0a3cf5ada
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-08-25 21:35:05 -0600

    adding first container guts! (#1)
    * adding first container guts!
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- manifest/scripts/prepare_manifests.py
+++ manifest/scripts/prepare_manifests.py
@@ -12,17 +12,20 @@ import subprocess
 import sys
 import os
 
+
 def recursive_find(base, pattern="*json"):
     for root, _, filenames in os.walk(base):
         for filename in fnmatch.filter(filenames, pattern):
             yield os.path.join(root, filename)
 
+
 def write_json(data, filename):
     """
     Write json to file
     """
-    with open(filename, 'w') as fd:
+    with open(filename, "w") as fd:
         fd.write(json.dumps(data, indent=4))
+
 
 def read_file(filename):
     """
@@ -32,6 +35,7 @@ def read_file(filename):
         content = fd.read()
     return content
 
+
 def read_json(input_file):
     """
     Read json
@@ -40,37 +44,38 @@ def read_json(input_file):
         data = json.loads(filey.read())
     return data
 
+
 class ManifestGenerator:
-    def __init__(self, outfile):
-        self.image = image
+    def __init__(self):
         self.check()
+        self.manifests = {}
 
     def check(self):
-        if not shutil.which('docker'):
-            sys.exit('docker not found, required for export.')
+        if not shutil.which("docker"):
+            sys.exit("docker not found, required for export.")
 
-    def run(self):
+    def run(self, image):
         """
         Run the generator to create a paths manifest.
         """
-        # Organized by URI for easier composition
-        manifests = {}
-        print(f"Adding {self.image} to paths manifest")
-        tmpdir = self.docker_export(self.image)
+        print(f"Adding {image} to paths manifest")
+        tmpdir = self.docker_export(image)
         paths = self.get_paths(os.path.join(tmpdir, "meta"))
-        print(f"\nSearching {self.image}")
-        manifests[self.image] = self.explore_paths(os.path.join(tmpdir, "root"), paths=paths)
-        shutil.rmtree(tmpdir)
-        return manifests
+        print(f"\nSearching {image}")
+        self.manifests[image] = self.explore_paths(
+            os.path.join(tmpdir, "root"), paths=paths
+        )
+        shutil.rmtree(tmpdir, ignore_errors=True)
+        return {image: self.manifests[image]}
 
     def explore_paths(self, root, paths):
         """
         Find executables under a path.
         """
-        manifest = {}        
+        manifest = {}
         for path in paths:
             files = set()
-            root_path = os.path.join(root, path.strip('/'))
+            root_path = os.path.join(root, path.strip("/"))
             if os.path.exists(root_path):
                 print(f"... {path}")
                 [files.add(x) for x in os.listdir(root_path)]
@@ -87,12 +92,18 @@ class ManifestGenerator:
             if "manifest" in jsonfile:
                 continue
             print("Found layer config %s" % jsonfile)
-            cfg = read_json(jsonfile)
-            for envar in cfg['container_config']['Env']:
-                print(envar)
+            data = read_json(jsonfile)
+
+            # Fallback to config
+            cfg = data.get("container_config") or data.get("config")
+            if not cfg or "Env" not in cfg:
+                continue
+
+            for envar in cfg.get("Env") or []:
                 if "PATH" in envar:
-                    envar = envar.replace('PATH=', '')
-                    for path in envar.split(':'):
+                    print(envar)
+                    envar = envar.replace(" ", "").replace("PATH=", "")
+                    for path in envar.split(":"):
                         paths.add(path)
         return paths
 
@@ -102,7 +113,7 @@ class ManifestGenerator:
         """
         if not tmpdir:
             tmpdir = tempfile.mkdtemp()
-        prefix = image.replace('/', '-').replace(':', '-')
+        prefix = image.replace("/", "-").replace(":", "-")
         save = os.path.join(tmpdir, f"{prefix}-save.tar")
         export = os.path.join(tmpdir, f"{prefix}.tar")
         export_dir = os.path.join(tmpdir, "root")
@@ -110,32 +121,46 @@ class ManifestGenerator:
 
         self.call(["docker", "pull", image])
 
-        self.call(["docker", "run", "--rm", "--name", prefix, "-d", image, "tail", "-f", "/dev/null"])
+        self.call(
+            [
+                "docker",
+                "run",
+                "--rm",
+                "--name",
+                prefix,
+                "--entrypoint",
+                "tail",
+                "-d",
+                image,
+                "-f",
+                "/dev/null",
+            ]
+        )
 
         # This is the filesystem
         self.call(["docker", "export", prefix, "--output", export])
-        
+
         # This will have the config
         self.call(["docker", "save", image, "--output", save])
         self.call(["docker", "stop", prefix])
         self.call(["docker", "rm", "--force", prefix])
         self.call(["docker", "rmi", "--force", image])
-        
+
         for tar in export, save:
             if not os.path.exists(tar):
                 sys.exit(f"There was an issue exporting/saving to {tar}")
 
-        for dirname in save_dir, export_dir:          
+        for dirname in save_dir, export_dir:
             os.makedirs(dirname)
-        self.call(["tar", "-xvf", export, "-C", export_dir])
-        self.call(["tar", "-xvf", save, "-C", save_dir])
+        self.call(["tar", "-xf", export, "-C", export_dir])
+        self.call(["tar", "-xf", save, "-C", save_dir])
         return tmpdir
-        
 
     def call(self, cmd):
         p = subprocess.call(cmd)
         if p != 0:
             sys.exit("Issue running command %s" % " ".join(cmd))
+
 
 def get_parser():
     parser = argparse.ArgumentParser(
@@ -143,14 +168,13 @@ def get_parser():
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
-        "--image",
+        "image",
         help="Container URI to parse",
     )
     parser.add_argument(
-        "-o"
+        "-o",
         "--outfile",
         help="Output manifest file",
-        default="paths-manifest.json",
         dest="outfile",
     )
     return parser
@@ -170,7 +194,11 @@ def main():
     gen = ManifestGenerator()
     manifests = gen.run(args.image)
     if args.outfile:
-        write_json(manifests, args.outfile)        
+        print(f"Saving to {args.outfile}...")
+        write_json(manifests, args.outfile)
+    else:
+        print(json.dumps(manifests, indent=4))
+
 
 if __name__ == "__main__":
     main()

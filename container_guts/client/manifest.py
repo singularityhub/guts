057658254ae6219270d8372066ed4961c9eae20a
vsoch <vsoch@users.noreply.github.com>
2022-08-27 16:29:24 -0600

    tweak
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- container_guts/client/manifest.py
+++ container_guts/client/manifest.py
@@ -23,7 +23,7 @@ def main(args, parser, extra, subparser):
 
     # Default to using outfile first, then outdir if defined
     if args.outfile:
-        outfile = args.outfile        
+        outfile = args.outfile
     elif args.outdir:
         outfile = os.path.join(args.outdir, "%s.json" % cli.save_path(args.image))
         dirname = os.path.dirname(outfile)
@@ -33,8 +33,7 @@ def main(args, parser, extra, subparser):
     # If we have an output file, make sure to set step output
     if outfile:
         print(f"Saving to {outfile}...")
-        print(f'echo "::set-output name=outfile::{outfile}")    
+        print(f'echo "::set-output name=outfile::{outfile}')
         utils.write_json(manifests, outfile)
     else:
         print(json.dumps(manifests, indent=4))
-        
